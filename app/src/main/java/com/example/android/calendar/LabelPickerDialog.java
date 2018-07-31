31f9d615ef38b69d95704a4fa0c88729cc289a8e
Asaf Kovrovski <35013356+AsafKov@users.noreply.github.com>
2018-07-31 21:24:04 +0300

    31/07/2018 - Improvments to UI (Mostly astherical) - Added smart-system that handles conflicting times of events - ... Some other stuff I can't recall - Should commit more often

---
+++ app/src/main/java/com/example/android/calendar/LabelPickerDialog.java
@@ -0,0 +1,37 @@
+package com.example.android.calendar;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+
+public class LabelPickerDialog extends DialogFragment {
+
+    public static final String EX_LABEL = "extraLabel";
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        super.onCreateDialog(savedInstanceState);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        alertDialogBuilder.setItems(Event.mLabelsTypes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                sendResult(Event.mLabelsTypes[which]);
+            }
+        });
+        alertDialogBuilder.setCustomTitle(getActivity().getLayoutInflater().inflate(R.layout.label_picker_headline, null));
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        return alertDialog;
+    }
+
+    private void sendResult(String type){
+        Intent intent = new Intent();
+        intent.putExtra(EX_LABEL, type);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, intent);
+    }
+}
